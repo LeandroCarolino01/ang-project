658512fb822300be44844f06ee106907aa133961
wagnerssouza <wagneersantos@gmail.com>
2020-06-18 13:10:05 -0300

    feat(breeds): create new method get all

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { DogsService } from '../../services/dogs.service';
+import { Component, OnInit } from "@angular/core";
+import { DogsService } from "../../services/dogs.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  data:Array<any>
-  constructor(private dog:DogsService) { }
+  data: Array<any>;
+  allBreeds: any;
+  constructor(private dog: DogsService) {}
 
   ngOnInit() {
+    this.getAllBreeds();
   }
-  show(){
+  show() {
     this.dog.getImages().subscribe((data) => {
-      console.log(data)
+      console.log(data);
       // this.data = data;
-     
-    })
+    });
+  }
+
+  getAllBreeds() {
+    this.dog.gelAllBreeds().subscribe(
+      (success: any) => {
+        this.allBreeds = success.message;
+      },
+      (error) => console.log(error)
+    );
   }
 }
